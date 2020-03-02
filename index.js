0b38c874a4eb82a223cc186db302559c4a794477
Kevin Peguero <kevinpeguero2@gmail.com>
2020-03-01 21:36:56 -0700

    HTML/INDEX.JS/STYLE.CSS

---
+++ index.js
@@ -0,0 +1,116 @@
+const weatherCard1 = $(".Weather-card-1")
+const weatherCard2 = $(".Weather-card-2")
+const weatherCard3 = $(".Weather-card-3")
+const weatherCard4 = $(".Weather-card-4")
+const weatherCard5 = $(".Weather-card-5")
+const historyEl = $(".history")
+
+const currentWeather = $(".Current-Weather")
+const inputSearch = $("#input-search-city")
+const searchButton = $("#submit")
+var currentCard = weatherCard1
+var localHistory = []
+var historyLoaded = false
+if (localStorage.getItem('localHistory')) {
+    var localHistory = JSON.parse(localStorage.getItem('localHistory'))
+    console.log(localHistory)
+    loadHistory()
+} else {
+    var localHistory = [];
+}
+console.log(localHistory)
+
+searchButton.click(function(e) {
+    e.preventDefault()
+    var city = inputSearch.val();
+    currentCard = weatherCard1;
+    getCurrentWeather(city)
+    getForecast(city)
+    localStorage.setItem("last-search",city)
+    if (!($('button').hasClass(`${city}`))){
+        localHistory.push(`${city}`)
+        localStorage.setItem("localHistory", JSON.stringify(localHistory))
+    }
+    loadHistory(city)
+})
+
+function getCurrentWeather(city) {
+    $.ajax({
+        url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=1c7ba168ec80866d4c17db02597dbb6f`,
+        method: "GET"
+
+    }).then(function (response) {
+        var date = new Date();
+        currentWeather.empty()
+        currentWeather.addClass('border');
+        currentWeather.append(
+            $(`<h1>${response.name}</h1>`),
+            $(`<h2>${date}</h2>`),
+            $(`<h3>${response.weather[0].description}</h3>`),
+            $(`<img src="http://openweathermap.org/img/w/${response.weather[0].icon}.png"></img>`),
+            $(`<h3>currentWeather ${response.main.temp}&#176</h3>`),
+            $(`<h3>humidity ${response.main.humidity}</h3>`),
+            $(`<h3>windSpeed ${response.wind.speed}</h3>`),
+        )
+        var lon = response.coord.lon;
+        var lat = response.coord.lat;
+
+        $.ajax({
+            url: `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&APPID=1c7ba168ec80866d4c17db02597dbb6f`,
+            method: "GET"
+
+        }).then(function (response) {
+            currentWeather.append(
+                $(`<h3>${response.value}`)
+            )
+        })
+    })
+}
+
+function getForecast(city) {
+    $.ajax({
+        url: `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&APPID=1c7ba168ec80866d4c17db02597dbb6f`,
+        method: "GET"
+
+    }).then(function (response) {
+        emptyCard();
+        for (let i = 0; i < 40; i += 8) {
+            currentCard.addClass('border')
+            currentCard.append(
+                $(`<p>${response.city.name}</p>`),
+                $(`<p>${moment(response.list[i].dt_txt).format("dddd")}</p>`),
+                $(`<p>${response.list[i].weather[0].description}</p>`),
+                $(`<img src="http://openweathermap.org/img/w/${response.list[i].weather[0].icon}.png"></img>`),
+                $(`<p>currentWeather ${response.list[i].main.temp}&#176</p>`),
+                $(`<p>humidity ${response.list[i].main.humidity}</p>`),
+                $(`<p>windSpeed ${response.list[i].wind.speed}</p>`),
+
+            )
+            currentCard = currentCard.next();
+            console.log(currentCard)
+
+        }
+    })
+}
+
+function emptyCard(){
+ weatherCard1.empty()
+ weatherCard2.empty()
+ weatherCard3.empty()
+ weatherCard4.empty()
+ weatherCard5.empty()
+}
+
+function loadHistory(city){
+    historyEl.empty()
+    if (historyLoaded === true) {
+    for(var i = 0; i < localHistory.length; i++){
+        historyEl.append($(`<button class="btn btn-outline-secondary ${city}">${localHistory[i]}</button>`))
+        }
+    } else if (historyLoaded === false) {
+        for(var i = 0; i < localHistory.length; i++){
+            historyEl.append($(`<button class="btn btn-outline-secondary ${localHistory[i]}">${localHistory[i]}</button>`))
+            }
+            historyLoaded = true
+    }
+}
